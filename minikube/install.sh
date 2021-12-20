c4cbd79917f77256edcf61f3420d140e37bf55a1
Nicolò Boschi <boschi1997@gmail.com>
2021-12-20 13:24:05 +0100

    install script

---
+++ minikube/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+grep '.pvtool/bin' $HOME/.bashrc ||  echo "PATH=\$PATH:$HOME/.pvtool/bin" >> "$HOME/.bashrc"
+mkdir -p $HOME/.pvtool
+mkdir -p $HOME/.pvtool/bin
+cd $HOME/.pvtool
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm repo add datastax-pulsar https://datastax.github.io/pulsar-helm-chart
+helm repo update
+
+curl -LO https://github.com/derailed/k9s/releases/download/v0.25.15/k9s_Linux_x86_64.tar.gz
+tar xzvf k9s_Linux_x86_64.tar.gz
+mv k9s bin/k9s
+
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+
---
+++ minikube/utils.sh
@@ -0,0 +1,9 @@
+
+pvt_clean() {
+    helm delete pvtpulsar
+    kubectl delete --all all
+}
+
+pvt_install() {
+    helm install --wait --debug --timeout 1200s pvtpulsar datastax-pulsar/pulsar --version=2.0.9  -f values272.yaml 
+}

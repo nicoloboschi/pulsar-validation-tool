92aa0261c18a0d133f3dc8fb1471fa5c8ea19b98
Nicolò Boschi <boschi1997@gmail.com>
2021-12-23 10:33:51 +0100

    make image parameteric

--- minikube/utils.sh
+++ minikube/utils.sh
@@ -13,3 +13,14 @@ pvt_install() {
     pvt_clean
     helm install --wait --debug --timeout 1200s pvtpulsar datastax-pulsar/pulsar --version=2.0.9  -f $1 
 }
+
+pvt_render() {
+    name=$(2:-$PVT_HOME/tmp/$1-$(uuidgen).yaml)
+    envsubst < $1 > $name
+}
+
+pvt_render_install() {
+    name=$PVT_HOME/tmp/$1-$(uuidgen).yaml
+    pvt_render $1 $name
+    pvt_install $name
+}

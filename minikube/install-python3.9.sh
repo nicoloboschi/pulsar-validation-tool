9f005d0c7f9f3944931a3adfeb919391ca735e80
Nicolò Boschi <boschi1997@gmail.com>
2022-03-07 15:38:53 +0100

    fix tweak itests

--- minikube/install-python3.9.sh
+++ minikube/install-python3.9.sh
@@ -5,6 +5,6 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt list | grep python3.9
 
-sudo apt-get install python3.9
+sudo apt-get install python3.9 -y
 sudo update-alternatives --config python3
 python3 -V

0606b060ca458c40d813b0ce0ea25ced5f14960b
Nicolò Boschi <boschi1997@gmail.com>
2022-03-16 14:14:05 +0100

    Elastic tweak

--- minikube/utils.sh
+++ minikube/utils.sh
@@ -11,7 +11,7 @@ pvt_clean() {
 
 pvt_install() {
     pvt_clean
-    helm install --wait --debug --timeout 1200s pvtpulsar datastax-pulsar/pulsar --version=2.0.9  -f $1 
+    helm install --wait --debug --timeout 1200s pvtpulsar datastax-pulsar/pulsar --version=2.0.14  -f $1 
 }
 
 pvt_render() {

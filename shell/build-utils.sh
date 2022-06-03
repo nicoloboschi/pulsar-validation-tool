13d5635ad27256f1e4ec262835632e8b978faf76
Nicolò Boschi <boschi1997@gmail.com>
2022-06-03 16:54:46 +0200

    build

--- shell/build-utils.sh
+++ shell/build-utils.sh
@@ -5,6 +5,9 @@ build_pulsar() {
 build_pulsar_core() {
     mvnd clean install -DskipTests -Dcheckstyle.skip -Dspotbugs.skip -Pcore-modules
 }
-build_pulsar_core_no_clean() {
+build_pulsar_core_no_mvnd() {
     mvn clean install -DskipTests -Dcheckstyle.skip -Dspotbugs.skip -Pcore-modules -T 1C
+}
+cleanup_docker() {
+    docker rmi -f $(docker images -q)
 }
